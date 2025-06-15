e535f4aad980ebdf755751c716fb6a07b3c86828
nanami1001 <5b1g0031@stust.edu.tw>
2025-06-15 18:31:44 +0800

    first commit

---
+++ kojin-web-ver3.02/js/index.js
@@ -0,0 +1,21 @@
+// 生成乘法表的函數
+function myFunction() {
+    var n = document.getElementById('numberInput').value;
+    var str = "<table>";
+
+    for (let i = 1; i <= n; i++) {
+        str += "<tr>";
+        for (let j = 1; j <= n; j++) {
+            // 如果是第一列（j == 1），套用藍色樣式
+            if (j === 1) {
+                str += `<td class="highlight"> ${i} * ${j} = ${i * j}</td>`;
+            } else {
+                str += `<td> ${i} * ${j} = ${i * j}</td>`;
+            }
+        }
+        str += "</tr>";
+    }
+    str += "</table>";
+
+    document.getElementById("myId").innerHTML = str;
+}
---
+++ kojin-web-ver3.02/js/sidemenu.js
@@ -0,0 +1,27 @@
+// sidemenu.js
+// 統一側邊欄開關邏輯
+const sidebar = document.getElementById("mySidebar");
+document.getElementById("menu-button").addEventListener("click", function () {
+    sidebar.style.width = "250px";
+});
+document.getElementById("close-button").addEventListener("click", function () {
+    sidebar.style.width = "0";
+});
+
+// 取得當前頁面 URL
+const currentPage = window.location.pathname.split("/").pop();
+
+// 定義對應的按鈕 ID
+const pageLinks = {
+    "index.html": "homeLink",
+    "about.html": "aboutLink",
+    "futrue.html": "futureLink",
+    "workmov.html": "workLink",
+    "other.html": "otherLink"
+};
+
+// 如果當前頁面對應到選單中的連結，則加上 active 類別
+if (pageLinks[currentPage]) {
+    const activeLink = document.getElementById(pageLinks[currentPage]);
+    if (activeLink) activeLink.classList.add("active");
+}
